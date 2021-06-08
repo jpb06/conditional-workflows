50ce08128f5fbabb4768116175daee655997cd83
jpb06 <jpb.06@outlook.com>
2021-06-08 16:02:12 +0200

    Fixing path

--- src/data-fetching/getUserBorrowedBooks.ts
+++ src/data-fetching/getUserBorrowedBooks.ts
@@ -3,7 +3,9 @@ import { readJson } from 'fs-extra';
 import { BorrowedBook } from '../types/data';
 
 export const getUserBorrowedBooks = async (idUser: number) => {
-  const books = (await readJson('./data/users.json')) as Array<BorrowedBook>;
+  const books = (await readJson(
+    './data/borrowed-books.json'
+  )) as Array<BorrowedBook>;
 
   return books.filter((el) => el.idUser === idUser);
 };
